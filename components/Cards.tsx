85d913e766e38087f8509d2b67b98e8654990672
your.user.name <erdiha@hotmail.com>
2022-11-10 16:48:10 -0800

    contact, about and footer sections added, scroll efffects added, resume added

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -38,7 +38,7 @@ function Cards(props: ICard) {
           <a
             href={props.code}
             target="_blank"
-             rel="noreferrer"
+            rel="noreferrer"
             className="inline-flex items-center py-2 px-3 text-sm 
         font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4
         focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700
