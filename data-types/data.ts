1e2d5b418a202654118b1359e4f276da52f49764
your.user.name <erdiha@hotmail.com>
2022-12-30 16:40:30 -0800

    Thoodies website added to projects

--- data-types/data.ts
+++ data-types/data.ts
@@ -1,43 +1,58 @@
 import { ICard } from './types';
 
-export const projectNames = ['MyFlix', 'MyCamper', 'Quiz Game', 'Cya App'];
+export const projectNames = [
+	'Thoodies',
+	'MyFlix',
+	'MyCamper',
+	'Quiz Game',
+	'Cya App',
+];
 export const projectLinks = [
-  'https://myflix-eight.vercel.app/',
-  'https://main--effortless-kangaroo-bb5cd4.netlify.app/',
-  'https://main.d2en2iuoalsx7.amplifyapp.com/',
-  'https://cya-client-cs130.herokuapp.com/login',
+	'https://main--fabulous-valkyrie-e036e0.netlify.app/',
+	'https://myflix-eight.vercel.app/',
+	'https://main--effortless-kangaroo-bb5cd4.netlify.app/',
+	'https://main.d2en2iuoalsx7.amplifyapp.com/',
+	'https://cya-client-cs130.herokuapp.com/login',
 ];
 export const projectDescriptions = [
-  'MyFlix is a Netflix mockup website I created using tailwind, NextJS, React, JS, and Firebase',
-  'MyCamper is an individualized park finder allowing users to search for and bookmark any national park in the US.',
-  'Quiz Game is a fun little side project, which can be played with one or two people.',
-  'Cya App is a scheduling app I created along with four of my classmates as our final project at UCLA.',
+	'Thoodies is a recipe website I created using NextJS, Tailwind, Typescript, and Firebase',
+	'MyFlix is a Netflix mockup website I created using tailwind, NextJS, React, TS, and Firebase',
+	'MyCamper is an individualized park finder allowing users to search for and bookmark any national park in the US.',
+	'Quiz Game is a fun little side project, which can be played with one or two people.',
+	'Cya App is a scheduling app I created along with four of my classmates as our final project at UCLA.',
 ];
 
 export const techUsed = [
-  ' React, Firebase, Recoil, Next.js, TypeScript, API, Material UI.',
-  '',
-  'JSX, React, SCSS',
-  '  JSX, Firebase, MongoDB, CSS, Bootstrap, Express.',
+	' React, Firebase, Recoil, Next.js, TypeScript, API, Material UI.',
+	'',
+	'JSX, React, SCSS',
+	'  JSX, Firebase, MongoDB, CSS, Bootstrap, Express.',
 ];
 
 export const projectsCode = [
-  'https://github.com/Erdiha/myFLIX',
-  'https://github.com/Erdiha/Camper',
-  'https://github.com/Erdiha/Quiz-Game',
-  'https://github.com/cs130-w22/Group-A1',
+	'https://github.com/Erdiha/Thoodies',
+	'https://github.com/Erdiha/myFLIX',
+	'https://github.com/Erdiha/Camper',
+	'https://github.com/Erdiha/Quiz-Game',
+	'https://github.com/cs130-w22/Group-A1',
 ];
-const images = ['myflix.jpg', 'parks.jpg', 'quiz.jpg', 'schedule.jpg'];
+const images = [
+	'recipe.jpg',
+	'myflix.jpg',
+	'parks.jpg',
+	'quiz.jpg',
+	'schedule.jpg',
+];
 
 export const allProj: any = [];
-for (let i = 0; i < 4; i++) {
-  const temp: ICard = {
-    id: i,
-    title: projectNames[i],
-    description: projectDescriptions[i],
-    link: projectLinks[i],
-    code: projectsCode[i],
-    image: images[i],
-  };
-  allProj.push(temp);
+for (let i = 0; i < 5; i++) {
+	const temp: ICard = {
+		id: i,
+		title: projectNames[i],
+		description: projectDescriptions[i],
+		link: projectLinks[i],
+		code: projectsCode[i],
+		image: images[i],
+	};
+	allProj.push(temp);
 }
